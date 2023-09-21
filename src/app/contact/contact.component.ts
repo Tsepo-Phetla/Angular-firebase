0d68ab3232fbf3ceb0982420e6d4e9b66653141e
TSEPO PHETLA <tsepophetla@gmail.com>
2023-09-21 20:42:49 +0200

    Emailjs

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
-
+import * as emailjs from 'emailjs-com';
+import { EmailJSResponseStatus } from 'emailjs-com';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  
+
 
   protected aFormGroup: FormGroup;
 
@@ -23,6 +26,14 @@ export class ContactComponent implements OnInit {
     });
   }
   siteKey:string = "6LdCwjYoAAAAAKm9ytee7i13p7SDjBohc3ZvObO3";
-
+ public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm('service_45rahm9', 'template_4m57urn', e.target as HTMLFormElement, 'd7LyN4AYEABUv-poy')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
 }
 
